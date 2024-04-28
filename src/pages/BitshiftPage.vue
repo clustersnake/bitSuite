6201ed0c6525fa313ddbdf8a709a80b13db0dcd4
Eloy Ramirez <clustersnake45@gmail.com>
2024-04-28 16:59:22 -0400

    feat: Import BitsArray component into pages

--- src/pages/BitshiftPage.vue
+++ src/pages/BitshiftPage.vue
@@ -1,19 +1,16 @@
 <template>
   <q-page class="q-px-md">
-    <h3 class="">{{ entrada }}</h3>
     <article>
+      <header>
+        <h3 v-if="entrada">{{ entrada }}</h3>
+        <h3 v-else>0</h3>
+      </header>
       <section>
         <q-input v-model.number="entrada" type="number" />
       </section>
       <section class="q-my-md">
         <header>Bits</header>
-        <q-btn
-          v-for="(bit, idx) in bitButtons"
-          :key="`bitBtn-${idx}`"
-          :label="bit"
-          :color="bit == '1' ? 'primary' : ''"
-          :text-color="bit == '0' ? 'black' : ''"
-        />
+        <bits-array :numbers="entrada"/>
       </section>
       <section>
         <q-btn icon="keyboard_double_arrow_left" @click="moveLeft" />
@@ -24,7 +21,8 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+import BitsArray from 'src/components/BitsArray.vue';
 
 defineOptions({
   name: 'bitShiftPage',
@@ -39,12 +37,4 @@ const moveRight = () => {
   if (entrada.value >= 2 ** 0) entrada.value >>= 1;
 };
 
-const bitButtons = computed(() => {
-  const num: number = entrada?.value || 0;
-
-  const val = num.toString(2);
-  if (val.length < 32) return '0'.repeat(32 - val.length) + val;
-
-  return val;
-});
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,40 +1,27 @@
 <template>
   <q-page class="q-px-md">
-    <h3 class="">{{ entrada }}</h3>
     <article>
+      <header>
+        <h3 v-if="entrada">{{ entrada }}</h3>
+        <h3 v-else>0</h3>
+      </header>
       <section>
         <q-input v-model.number="entrada" type="number" />
       </section>
       <section class="q-my-md">
         <header>Bits</header>
-        <q-btn
-          v-for="(bit, idx) in bitButtons"
-          :key="`bitBtn-${idx}`"
-          :label="bit"
-          :color="bit == '1' ? 'primary' : ''"
-          :text-color="bit == '0' ? 'black' : ''"
-        />
+        <bits-array :numbers="entrada" />
       </section>
     </article>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-// import { Todo, Meta } from 'components/models';
-// import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from 'vue';
+import BitsArray from 'src/components/BitsArray.vue';
 
 defineOptions({
   name: 'IndexPage',
 });
-const entrada = ref<number>(1);
-
-const bitButtons = computed(() => {
-  const num: number = entrada?.value || 0;
-
-  const val = num.toString(2);
-  if (val.length < 32) return '0'.repeat(32 - val.length) + val;
-
-  return val;
-});
+const entrada = ref<number>(0);
 </script>
